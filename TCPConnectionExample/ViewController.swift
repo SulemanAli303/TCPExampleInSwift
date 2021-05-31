619cee8504b8dae276adba75c315f8f1f88db45d
@sulemanalibuiltin <suleman.ali@builtinsoft.com>
2021-05-31 16:01:07 +0500

    Initial Commit

---
+++ TCPConnectionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TCPConnectionExample
+//
+//  Created by Suleman Ali on 31/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
